86ed82e3a11de20fb20a43308f1b7082b53254a6
Santiago Peña Nieto <santip2357@gmail.com>
2024-08-23 16:19:43 -0500

    Implemented the getText function in class Line

--- line.py
+++ line.py
@@ -1,5 +1,16 @@
 from sys import stdin
 
 class Line:
-    def __init__(self, text):
-        self.text = text
+    def __init__(self, textPath, type, lineNumber):
+        self.textPath = textPath
+        self.type = type
+        self.lineNumber = lineNumber
+        self.text = self.getText(self)
+    
+    def getText(self):
+        if self.type:
+            path = "history.txt"
+        else:
+            path = "dialogue.txt"
+        with open(path, 'r') as file:
+            return file[self.lineNumber]
